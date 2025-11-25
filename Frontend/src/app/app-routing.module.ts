ac9618bee7b01265633d78091a399fcb6d3cbb85
grendleard <grendleard@gmail.com>
2025-11-25 17:23:11 -0500

    feat: implement routing navigation for customers, products, and invoices

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { InvoiceListComponent } from './components/invoice-list/invoice-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customers', component: CustomerListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'invoices', component: InvoiceListComponent },
+  { path: '', redirectTo: '/customer', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { CustomerListComponent } from './components/customer-list/customer-list.component';
 import { MatTableModule } from '@angular/material/table';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { InvoiceListComponent } from './components/invoice-list/invoice-list.component';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
     AppComponent,
     CustomerListComponent,
     ProductListComponent,
+    InvoiceListComponent,
   ],
   imports: [
     BrowserModule,
